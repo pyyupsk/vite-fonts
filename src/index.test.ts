821105d27f0f0039a588aa0a229be640cbf59c04
Pongsakorn Thipayanate <166195305+pyyupsk@users.noreply.github.com>
2026-06-10 19:31:44 +0700

    chore: setup vitest with smoke test

---
+++ src/index.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, it } from 'vitest'
+
+import { hello } from './index'
+
+describe('hello', () => {
+  it('returns greeting', () => {
+    expect(hello('world')).toBe('Hello, world!')
+  })
+})
